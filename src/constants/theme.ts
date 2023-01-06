655a3443520b780e9720f3049f7fb16a0d499790
andriy <Andruxan1989@gmail.com>
2023-01-06 13:20:29 +0200

    start

---
+++ src/constants/theme.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  greenPrimary: "#85dbd0",
+  greenSecondary: "#6ACDA4",
+  greenDarkest: "#78D6CB",
+  greenLightest: "#aee3dd",
+  greenLight: "#ebfbf6",
+  light: "#e8f7f7",
+  lightGrey: "#cecece",
+  white: "#FFFFFF",
+};
